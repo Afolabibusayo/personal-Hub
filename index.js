4bc15cb67b2e1e309f196dc0ca5d026dd9f891bc
Afolabibusayo <132835478+Afolabibusayo@users.noreply.github.com>
2025-02-17 13:45:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function ShowSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function HideSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
