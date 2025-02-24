fa9e65bc5356e3699926824e568f03b7542fe9b9
Afolabibusayo <132835478+Afolabibusayo@users.noreply.github.com>
2025-02-24 23:38:48 +0100

    second commit

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function HideSideBar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+window.addEventListener("scroll", function () {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 100) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
